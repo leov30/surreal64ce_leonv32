aa29e02de783486d79ad2e349dac7adde9fb540b
leov30 <leo_v30@hotmail.com>
2022-11-09 13:49:56 -0800

    Add files via upload

---
+++ tools/not_in_surreal64.bat
@@ -0,0 +1,23 @@
+
+md _NOT_SURREAL64
+move "FIFA Soccer 64 (USA) (En,Fr,De).zip" _NOT_SURREAL64
+move "GameBooster 64 (USA) (v1.1) (Unl).zip" _NOT_SURREAL64
+move "Nintendo 64 Modem (Japan) (Add-on).zip" _NOT_SURREAL64
+move "South Park - Chef's Luv Shack (USA).zip" _NOT_SURREAL64
+move "SpaceStation Silicon Valley (USA) (Rev 1).zip" _NOT_SURREAL64
+move "SpaceStation Silicon Valley (USA).zip" _NOT_SURREAL64
+move "Turok - Dinosaur Hunter (Europe) (Rev 1).zip" _NOT_SURREAL64
+move "Turok - Dinosaur Hunter (Germany) (Rev 2).zip" _NOT_SURREAL64
+move "Turok - Dinosaur Hunter (USA) (Rev 1).zip" _NOT_SURREAL64
+move "Turok - Dinosaur Hunter (USA) (Rev 2).zip" _NOT_SURREAL64
+move "Turok 2 - Seeds of Evil (USA) (Demo) (Kiosk).zip" _NOT_SURREAL64
+move "Turok 2 - Seeds of Evil (USA) (Rev 1).zip" _NOT_SURREAL64
+move "Wetrix (USA) (En,Fr,De,Es,It,Nl).zip" _NOT_SURREAL64
+move "Zelda no Densetsu - Toki no Ocarina (Japan) (Rev 1).zip" _NOT_SURREAL64
+move "Zelda no Densetsu - Toki no Ocarina (Japan) (Rev 2).zip" _NOT_SURREAL64
+move "Zelda no Densetsu - Toki no Ocarina (Japan).zip" _NOT_SURREAL64
+move "Blues Brothers 2000 (Europe) (En,Fr,De,Es,It,Nl) (Beta) (2000-01-15) (#53).zip" _NOT_SURREAL64
+move "F-1 World Grand Prix (Europe) (Beta).zip" _NOT_SURREAL64
+move "F-Zero X (USA) (Beta) (The Legend of Zelda Ocarina of Time Overdump).zip" _NOT_SURREAL64
+move "Mortal Kombat Trilogy (USA) (Beta) (1996-05-13).zip" _NOT_SURREAL64
+move "Turok 3 - Shadow of Oblivion (USA) (Beta) (2000-07-21).zip" _NOT_SURREAL64
