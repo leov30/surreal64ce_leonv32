2223b4406ff1e3539a2f1094eb2169eea42c7861
leov30 <leo_v30@hotmail.com>
2023-01-27 19:11:19 -0800

    Add files via upload

--- tools/not_in_surreal64.bat
+++ tools/not_in_surreal64.bat
@@ -1,11 +1,9 @@
-
 md _NOT_SURREAL64
 move "FIFA Soccer 64 (USA) (En,Fr,De).zip" _NOT_SURREAL64
 move "GameBooster 64 (USA) (v1.1) (Unl).zip" _NOT_SURREAL64
 move "Nintendo 64 Modem (Japan) (Add-on).zip" _NOT_SURREAL64
 move "South Park - Chef's Luv Shack (USA).zip" _NOT_SURREAL64
-
-move "SpaceStation Silicon Valley (USA).zip" _NOT_SURREAL64
+move "SpaceStation Silicon Valley (USA) (Rev 1).zip" _NOT_SURREAL64
 move "Turok - Dinosaur Hunter (Europe) (Rev 1).zip" _NOT_SURREAL64
 move "Turok - Dinosaur Hunter (Germany) (Rev 2).zip" _NOT_SURREAL64
 move "Turok - Dinosaur Hunter (USA) (Rev 1).zip" _NOT_SURREAL64
@@ -22,4 +20,6 @@ move "F-Zero X (USA) (Beta) (The Legend of Zelda Ocarina of Time Overdump).zip" 
 move "Mortal Kombat Trilogy (USA) (Beta) (1996-05-13).zip" _NOT_SURREAL64
 move "Turok 3 - Shadow of Oblivion (USA) (Beta) (2000-07-21).zip" _NOT_SURREAL64
 move "Xplorer 64 (Germany) (v1.067) (Unl).zip" _NOT_SURREAL64
+move "SpaceStation Silicon Valley (Japan) (Proto).zip" _NOT_SURREAL64
+
 
